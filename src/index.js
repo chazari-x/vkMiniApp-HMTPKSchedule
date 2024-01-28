92f68301036f950dfd73bbfca87b4de1d8d3b563
chazari <chalimovrz@gmail.com>
2024-01-28 23:07:40 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 // Init VK Mini App
 bridge.send("VKWebAppInit").then(r =>  console.log(r));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: _ }) => {}); //runtime download
 }
